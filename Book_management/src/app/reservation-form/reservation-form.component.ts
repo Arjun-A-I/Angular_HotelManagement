3b4b153059855c2dc30e43457daa611ad2d81759
Arjun-A-I <arjunai5768@gmail.com>
2024-06-23 18:40:16 +0530

    Form validation done

--- Book_management/src/app/reservation-form/reservation-form.component.ts
+++ Book_management/src/app/reservation-form/reservation-form.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup,FormBuilder,Validators, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
-  // standalone: true,
-  // imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule,FormsModule],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
 export class ReservationFormComponent {
-
+reservationForm : FormGroup = new FormGroup({});
 }
--- Book_management/src/app/reservation/reservation.module.ts
+++ Book_management/src/app/reservation/reservation.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
-import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 
 
 
 @NgModule({
-  declarations: [ReservationFormComponent,ReservationListComponent],
+  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    
   ]
 })
 export class ReservationModule { }
